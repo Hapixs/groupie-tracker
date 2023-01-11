e6efebea0ce1b589025d1353d9d1761605a01ee7
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-01-11 11:31:23 +0100

    Add function theme color (not fonctionnal right now)

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,10 @@ function CloseMessage (id) {
     // Display none div click
     document.getElementById(id).style.display = "none";
 }
+
+// function ChangeTheme() {
+//     // Get theme
+//     var theme = document.getElementById("theme").value;
+//     // Set theme
+//     document.getElementById("theme").href = "static/css/" + theme + ".css";
+// }
