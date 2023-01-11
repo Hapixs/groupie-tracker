038664dd792931a5d3349ef135f30a0fe57b4235
salexandre <alexandre.sarouille@ynov.com>
2023-01-11 09:32:10 +0100

    fix info & error message display

--- handlers/errorHandler.go
+++ handlers/errorHandler.go
@@ -7,7 +7,9 @@ import (
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(notfoundTempaltePath))
-	data := HtmlData{}
+	data := HtmlData{
+		ErrorMessage: "La page demandé n'a pas été trouvé",
+	}
 	PrepareDataWithFragments(&data)
 	tmpl.Execute(w, data)
 }
--- handlers/homeHandler.go
+++ handlers/homeHandler.go
@@ -10,7 +10,6 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(homeTemplatePath))
 	data := HtmlData{
 		Groups: api.GetCachedGroups(),
-		Test:   "yep, fragments are working ! (and with variables ! :D) ",
 	}
 	PrepareDataWithFragments(&data)
 	tmpl.Execute(w, data)
