3d87d0a170cb18e02f1eff7cb97b893f3b19a8cc
Wiibleyde <nathan@bonnell.fr>
2023-01-17 16:46:26 +0100

    Add func get page link wiki

--- api/wikipediaapi.go
+++ api/wikipediaapi.go
@@ -44,3 +44,31 @@ func GetWikipediaImage(artist string) WikiRequest {
 	}
 	return request
 }
+
+func GetWikipediaPageLink(artist string) string {
+	artist = RemoveAccents(artist)
+	artist = strings.Join(strings.Split(artist, " "), "%20")
+	rand.Seed(time.Now().UnixMilli())
+	time.Sleep(time.Duration(rand.Intn(150)))
+	url := "https://en.wikipedia.org/w/api.php?action=query&titles=" + artist + "&prop=info&inprop=url&format=json"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var request WikiRequest
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		println("Error when parsing wikipedia api response for " + artist)
+		println(err.Error())
+		return ""
+	}
+	for _, page := range request.Query.Page {
+		return page.Thumbnail.Source
+	}
+	return ""
+}
