1a735a7906e8fab455fe214dfa859246a688c34c
salexandre <alexandre.sarouille@ynov.com>
2023-01-20 13:51:16 +0100

    c'est ca faire du responsive fonctionelle

--- static/js/googlemap.js
+++ static/js/googlemap.js
@@ -4,15 +4,22 @@
     let lo = map.getAttribute("data-lng");
     var myLatLng = new google.maps.LatLng(la, lo);
     var options = {     
-        zoom: 10,
+        zoom: 15,
         center: myLatLng,
-        scrollwheel: false
+        panControl:false,
+        zoomControl:false,
+        mapTypeControl:false,
+        scaleControl:false,
+        streetViewControl:false,
+        overviewMapControl:false,
+        rotateControl:false,    
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     map = new google.maps.Map(map, options);
     var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map
-    });
+      position: myLatLng,
+      map: map,
+  });
 }
 
 function ChangeMapCoordinates(la, lo) {
